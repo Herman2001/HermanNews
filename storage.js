ca3d917bc489286e95f9b9812896828fb8f87357
Herman2001 <102909811+Herman2001@users.noreply.github.com>
2026-05-11 21:11:45 +0200

    add localStorage hantering

---
+++ storage.js
@@ -0,0 +1,38 @@
+const STORAGE_KEY = 'herman_articles';
+
+/**
+ * Hämtar alla artiklar från localStorage
+ */
+export function getArticles() {
+  return JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
+}
+
+/**
+ * Sparar hela arrayen med artiklar till localStorage
+ */
+export function saveArticles(articles) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(articles));
+}
+
+/**
+ * Hämtar en specifik artikel baserat på ID
+ */
+export function getArticleById(id) {
+  return getArticles().find(a => a.id === id) || null;
+}
+
+/**
+ * Uppdaterar en befintlig artikel eller lägger till en ny om ID saknas
+ */
+export function updateArticle(updatedArticle) {
+  const articles = getArticles();
+  const index = articles.findIndex(a => a.id === updatedArticle.id);
+  
+  if (index !== -1) {
+    articles[index] = updatedArticle;
+  } else {
+    articles.unshift(updatedArticle);
+  }
+  
+  saveArticles(articles);
+}
